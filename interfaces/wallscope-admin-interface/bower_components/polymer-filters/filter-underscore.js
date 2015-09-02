36808d6a2e31291809740f856085ed9507a50b5d
Anthony Perritano <aperritano@gmail.com>
2015-09-02 00:41:58 -0500

    separated all the apps

--- interfaces/wallscope-admin-interface/bower_components/polymer-filters/filter-underscore.js
+++
@@ -1,8 +0,0 @@
-/**
- * Return  Underscored `str`.
- * @param  {string} str 
- * @return {string}
- */
-PolymerExpressions.prototype.underscore = function(str){
-  return str.replace(/([a-z\d])([A-Z])/g, '$1_$2').toLowerCase();
-};
