7a7ed5ad4e92bf7ae970d00f6468b995a1f74dd5
Anthony Perritano <aperritano@gmail.com>
2015-09-24 12:03:19 -0500

    added configuration for tom

--- bots/configuration/node-bot.js
+++ bots/configuration/node-bot.js
@@ -1,6 +1,7 @@
 
 var NUTELLA = require('nutella_lib');
 var masterConfiguration = require('./master_configuration.json');
+var simulatorConfiguration = require('./simulator_configuration.json');
 
 // Get configuration parameters and init nutella
 var cliArgs = NUTELLA.parseArgs();
@@ -21,4 +22,9 @@ for (var i = 0; i < masterConfiguration.habitats.length; i++) {
 nutella.net.handle_requests('master_configuration', function(message, from) {
     console.log('MasterConfiguration Requested');
     return masterConfiguration;
+});
+
+nutella.net.handle_requests('simulator_configuration', function(message, from) {
+    console.log('SimulatorConfiguration Requested');
+    return SimulatorConfiguration;
 });
