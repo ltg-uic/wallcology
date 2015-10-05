840d95ee30e295adaf2ff8486f6d318c44917ff8
tmoher <moher@toms-laptop.local>
2015-10-05 11:25:27 -0500

    history 0.81. bug fix on return of histories less than 25 point

--- bots/history/node-bot.js
+++ bots/history/node-bot.js
@@ -20,7 +20,7 @@ var history = nutella.persist.getMongoObjectStore('history');
 // because the existing history needs to be loaded
 // before any of the other handlers can fire
 //
-console.log("History version 0.8");
+console.log("History version 0.81");
 history.load(function(){
 
 // if there is no history db, initialize it here.
@@ -87,6 +87,8 @@ history.load(function(){
   //
   // we'll build an array B of length n that interpolates the data points in A, then return B
   //
+
+  if (A.length < n) return(A);
 
   var B = [];
 
