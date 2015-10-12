5b8abc0a5851207500b37ff3a83f65baafdd041f
Kyle Almryde <kyle.almryde@gmail.com>
2015-10-11 22:06:18 -0500

    added visualization interface

---
+++ interfaces/visualization/js/app/app.js
@@ -0,0 +1,334 @@
+
+/*==============================================================================
+ #    Program Name: App.js
+ #          Author: Kyle Reese Almryde
+ #            Date: 10/10/2015
+ #
+ #     Description:
+ #        Establishes and communicates between the Unity3D visualization and
+ #        Nutella. Handles updates from the simulation and passes them along to
+ #        the Unity visualization.
+ #
+ #    Version: 0.2.1
+ #
+ #    Deficiencies:
+ #       None. This program meets specifications
+ #
+ #==============================================================================
+ #                            GLOBAL VAR DECLARATIONS
+ #=============================================================================*/
+
+var unity3d,
+    wallscopeID;
+
+var SpeciesCounter = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];  // Keeps track of the current population counts
+
+/*==============================================================================
+ #                            FUNCTION DEFINITIONS
+ #=============================================================================*/
+
+// Instantiate the Nutella components
+function initNutellaComponents()
+{
+    console.log("Connecting Nutella");
+    // Parse the query parameters
+    var query_parameters = NUTELLA.parseURLParameters();
+    console.log(query_parameters);
+    wallscopeID = query_parameters.wallscope || 1;
+
+    console.log('wallscope: ' + wallscopeID)
+
+    // Get an instance of nutella.
+    nutella = NUTELLA.init(
+        query_parameters.broker,
+        query_parameters.app_id,
+        query_parameters.run_id,
+        NUTELLA.parseComponentId()
+    );
+
+    // (Optional) Set the resourceId
+    nutella.setResourceId('wallscope_vis');
+
+    // Subscribe to the Channel, assign callback function
+    subscribeToChannel('state_update', State_Update_Handler);
+    subscribeToChannel('species_event', Species_Event_Handler);
+
+
+    // initialize the unity WebPlayer
+    initWebPlayer();
+
+};
+
+
+function initWebPlayer()
+{
+    var config =
+    {
+        width: window.innerWidth, // 960,
+        height: window.innerHeight, // 600,
+        params: { enableDebugging:"0" }
+    };
+
+    unity3d = new UnityObject2(config);
+
+    var url = "build/wallscope" + wallscopeID.toString() + ".unity3d";
+
+    console.log("Start WebPlayer");
+    jQuery(function() {
+
+        var $missingScreen = jQuery("#unityPlayer").find(".missing");
+        var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+        $missingScreen.hide();
+        $brokenScreen.hide();
+
+        var installPlugin = function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            unity3d.installPlugin();
+            return false;
+        };
+
+        unity3d.observeProgress(function (progress) {
+            switch(progress.pluginStatus) {
+                case "broken":
+                    $brokenScreen.find("a").click(installPlugin);
+                    $brokenScreen.show();
+                break;
+                case "missing":
+                    $missingScreen.find("a").click(installPlugin);
+                    $missingScreen.show();
+                break;
+                case "installed":
+                    $missingScreen.remove();
+                break;
+                case "first":
+                break;
+            }
+        });
+        console.log("url is", url);
+        // recieve wallscope #, grab appropriate program.
+        unity3d.initPlugin(jQuery("#unityPlayer")[0], url);
+    });
+};
+
+
+/*==============================================================================
+ #                       NUTELLA MESSAGE HANDLERS
+ #=============================================================================*/
+
+/*
+    {
+        "timestamp":1444507362765,
+        "populations":[
+            [ 0,
+              0.5575783036839,
+              1.0272956989878372,
+              0,
+              0,
+              25.642044808154427,
+              1.267141699397365,
+              0,
+              0.5329095186830878,
+              38.697148131825934,
+              25.96008723053049
+            ],
+            [0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0]
+        ],
+        "environments":[
+            [  20.12000000000002,
+               1,
+               0.4,
+               -1
+            ],
+            [20.12000000000002,1,0.4,-1],
+            [20.12000000000002,1,0.4,-1],
+            [20.12000000000002,1,0.4,-1]
+        ]
+    };
+*/
+
+// Handles most recent state of simulation
+function Last_State_Handler( response )
+{
+    // Send messages to Unity
+    console.log("last_state!", response);
+
+    // Since they are doing the same thing...
+    State_Update_Handler( response, null );
+}
+
+
+function Species_Event_Handler( message, from )
+{
+    // ['increase','decrease','colonize','kill'];
+    console.log("Species_Event_Handler");
+    State_Update_Handler(message, from);
+
+}
+
+
+function State_Update_Handler(message, from)
+{
+
+    console.log('THE MESSAGE', message, "@", Date(message["timestamp"]));
+
+    // Send messages to Unity
+    SetThermometerText( message['environments'][wallscopeID-1][0] );  // Set the wallscope Temperature
+    UpdatePopulations(message['populations'][wallscopeID-1]);
+}
+
+/*==============================================================================
+ #                          UTILITY FUNCTIONS
+ #=============================================================================*/
+// Helper function to subscribe to channels
+function subscribeToChannel(channelName, messageHandler)
+{
+    console.log("subscribing to channel:", channelName);
+    nutella.net.subscribe(channelName, messageHandler);
+};
+
+
+
+// Manage population updates.
+function UpdatePopulations(population)
+{
+    console.log("UpdatePopulations!", population);
+
+    for (var i = 0; i < population.length; i++) {
+        var count = population[i]
+        console.log("\tcount is", count);
+        var delay = count % 2;
+        count = (count < 1 && count > 0)? Math.round(count * 3) : Math.round(count);
+        console.log("\t", i, "Now count is..", count);
+
+        // RequestPopulationCount(i.toString());
+        // console.log("\tCount and SpeciesCounter[i]", i, count,  SpeciesCounter[i] );
+        switch(i)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 6:
+            case 7:
+            case 8:
+                if ( count < SpeciesCounter[i] ) {
+
+                    while ( count < SpeciesCounter[i] ) {
+                        console.log("\tKill it!", i, count,  SpeciesCounter[i] );
+                        KillCritter(i);
+                        SpeciesCounter[i]--;
+                    }
+        //
+                } else if ( count > SpeciesCounter[i] ) {
+
+                    while ( count > SpeciesCounter[i] ) {
+                        console.log("\tSpawn it!", i, count,  SpeciesCounter[i] );
+                        // SpeciesCounter[i]++;
+                        SpawnCritter(i);
+                    }
+
+                } else {
+                    console.log("They are both zero");
+                };
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 9:
+            case 10:
+                console.log("Not Implemented yet!");
+                break;
+            default:
+                console.log("No critter Ive ever heard of");
+                break;
+        }
+    }
+    console.log("Thats a wrap!", SpeciesCounter);
+}
+
+
+/*==============================================================================
+ #                       UNITY MESSAGE HANDLERS
+ #=============================================================================*/
+ // These functions are called from WITHIN Unity!
+
+// Allows Unity to tell us its ready
+function initWallScopeStartState( live )
+{
+    console.log("Unity is READY!!", live)
+
+    // Make asynchronous requests on a certain channel
+    nutella.net.request( 'last_state', 'last_state', Last_State_Handler);
+}
+
+
+// Function is called by Unity, which gives it the species ID, and the number of
+// Unity GameObjects with that Identifier that
+function ReceivePopulationCount( uID, pCount )
+{
+    console.log("Species ID is", uID, "and there are", pCount );
+}
+
+
+function ProgressUpdate(func, valid)
+{
+    console.log(func, 'has executed', valid);
+}
+/*==============================================================================
+ #                       UNITY MESSAGE REQUESTS
+ #=============================================================================*/
+// These functions Send messages TO Unity
+
+
+// Set the Temperature Thermometer
+//     expects a float/double
+function SetThermometerText (temp)
+{
+    temp = Math.round(temp * 100) / 100;
+    temp = temp.toString() + "˚C";
+
+    console.log("Temperature is", temp);
+    unity3d.getUnity().SendMessage("Temperature", "SetText", temp);
+}
+
+
+// Spawns a critter
+// id is assumed to be an integer
+function SpawnCritter ( id )
+{
+    console.log("Lets make a ", typeof id, "!");
+    unity3d.getUnity().SendMessage( "WallScope", "SpawnCritter", id );
+    SpeciesCounter[id]++;
+    console.log("We have", SpeciesCounter[id], "of species", id, "now");
+}
+
+function KillCritter ( id )
+{
+    console.log("Killing", id, "softly!");
+    console.log("NOT IMPLEMENTED YET!");
+    // unity3d.getUnity().SendMessage("WallScope", "KillCritter", id);
+    SpeciesCounter[i]--;
+}
+
+
+// Calls UNITY requesting Population
+function RequestPopulationCount( uID )
+{
+    console.log("Make Request!", uID );
+    unity3d.getUnity().SendMessage("WallScope", "GetPopulationCount", uID);
+}
+
+
+
+
+
+/*==============================================================================
+ #                             START OF MAIN
+ #=============================================================================*/
+
+function Start() {
+    initNutellaComponents();
+}
