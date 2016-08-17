d8bffb75ec4661e3a2a885bb534d6e77b3dd8069
tmoher <moher@toms-laptop.local>
2016-08-17 14:43:29 -0500

    Species bot update.

--- bots/species-notes/node-bot.js
+++ bots/species-notes/node-bot.js
@@ -24,28 +24,29 @@ Notes.load(function(){
 
   //returns all notes for from a group index, e.g.,  group == 1
   nutella.net.handle_requests('all_notes_with_group',function(message, from) {
-    if (message !== null && message !== undefined) {
       // if coming from the debugger
       //var parsedNote = JSON.parse(message);
       var parsedNote = message;
-      console.log('request all_notes_with_group groupIndex: ' + parsedNote.group);
-      return (
-        Notes.notes.filter(
-        function(note){
-          return (note.group == parsedNote.group);
-        }));
-    } else {
-      return {};
-    }
+
+      if (!isNaN(parsedNote) && (parsedNote >= 0 && parsedNote <=4)) {
+        console.log('request all_notes_with_group groupIndex: ' + parsedNote.group);
+        return (
+          Notes.notes.filter(
+            function(note){
+            return (note.group == parsedNote.group);
+            }));
+      } else {
+        return {};
+      }
   });
 
 
   //returns all the species from a group index e.g., species == 2
   nutella.net.handle_requests('all_notes_with_species',function(message, from) {
-    if (message !== null && message !== undefined) {
-      // if coming from the debugger
+       // if coming from the debugger
       //var parsedNote = JSON.parse(message);
-      var parsedNote = message;
+    var parsedNote = message;
+    if (!isNaN(parsedNote) && (parsedNote >= 0 && parsedNote <=10)) {
       console.log('request all_notes_with_species speciesIndex: ' + parsedNote.species);
       return (
         Notes.notes.filter(
@@ -57,36 +58,55 @@ Notes.load(function(){
     }
   });
 
-  nutella.net.handle_requests('note_with_species_group',function(selector, from) { 
-     return(Notes.notes.filter(function(note){return (note.species == selector.species && note.group == selector.group)}));
-  });
+  nutella.net.handle_requests('note_with_species_group',function(message, from) { 
+       // if coming from the debugger
+      //var parsedNote = JSON.parse(message);
+    var parsedNote = message; 
+    if (!isNaN(parsedNote.group) && (parsedNote.group >= 0 && parsedNote.group <=4) &&
+        !isNaN(parsedNote.species) && (parsedNote.species >= 0 && parsedNote.species <=10)) {
+          console.log('request note_with_species_group speciesIndex: ' + parsedNote.species + ' groupIndex: ' + parsedNote.group);
+
+          var n = Notes.notes.filter(function(note){return (note.species == parsedNote.species && note.group == parsedNote.group)});
+          return ((n.length == 0) ? {} : n[n.length-1]);
+
+
+          // tony, your code below returns an array of notes, not a single note
+          // my strategy is to filter and then choose the last (most recent) one
+          // if there are more than one. there will never be more than one, though.
+          //
+          // return (
+          //   Notes.notes.filter(
+          //   function(note){
+          //     return (note.species == parsedNote.species);
+          //   }));
+
+    } else {
+      return {};
+    }
+});
+
  
-  // nutella.net.handle_requests('note_with_species_group',function(message, from) {
-  //   if (message !== null && message !== undefined) {
-  //     // if coming from the debugger
-  //     //var parsedNote = JSON.parse(message);
-  //     var parsedNote = message;
-  //     console.log('request note_with_species_group speciesIndex: ' + parsedNote.species + ' groupIndex: ' + parsedNote.group);
-  //     return (
-  //       Notes.notes.filter(
-  //       function(note){
-  //         return (note.species == parsedNote.species && note.group == parsedNote.group);
-  //       }));
-  //   } else {
-  //     return {};
-  //   }
-  // });
 
-
-  nutella.net.subscribe('save_note',function(note){ 
+  nutella.net.handle_requests('save_note',function(message){ 
+       // if coming from the debugger
+      //var parsedNote = JSON.parse(message);
+    var parsedNote=message;
+    console.log('request save_note: ' + parsedNote.group);
     // replace the var n declaration below with this one to maintain full history of species note updates, at performance cost
     // var n=Notes.notes;
-    var n = Notes.notes.filter(function(item){return (!(note.species == item.species && note.group == item.group))});
-    var d = new Date();
-    note['timestamp'] = d.getTime();
-    Notes.notes = n.push(note);
-    Notes.save();
-  });
+    if (!isNaN(parsedNote.group) && (parsedNote.group >= 0 && parsedNote.group <=4) &&
+        !isNaN(parsedNote.species) && (parsedNote.species >= 0 && parsedNote.species <=10)) {
+          var n = Notes.notes.filter(function(item){return (!(parsedNote.species == item.species && parsedNote.group == item.group))});
+          var d = new Date();
+          parsedNote['timestamp'] = d.getTime();
+          Notes.notes = n.push(parsedNote);
+          Notes.save();
+          nutella.net.publish('note_changes',parsedNote);
+          return parsedNote;
+     } else {
+      return {};
+     }
+   });
 
 
   nutella.net.subscribe('reset_notes',function(message,from) {
