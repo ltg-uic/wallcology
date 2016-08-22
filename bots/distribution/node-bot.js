dd53d83ca647ebf81ac15bc868a32719e2f5e8e2
tmoher <moher@toms-laptop.local>
2016-08-22 14:26:01 -0500

    V0.5 Questions, and sync for kyle

--- bots/distribution/node-bot.js
+++ bots/distribution/node-bot.js
@@ -16,11 +16,47 @@ channels.load(function(){
   if (!channels.hasOwnProperty('channel')){
     reset_channels();
   };
-  nutella.net.handle_requests('channel_print_names', function (message, from){
+  nutella.net.handle_requests('channel_names', function (message, from){
     return channels.channel;
   });
-
+  nutella.net.subscribe('set_channel_names', function (message, from){
+    channels.channel=message;
+    channels.save();
+  });
 });
+
+var roster = nutella.persist.getMongoObjectStore('roster');
+
+  roster.load(function(){
+  		// if (!roster.hasOwnProperty('logins')){
+    // 		reset_roster();
+  		// };
+  		nutella.net.handle_requests('roster', function (message, from){
+  			console.log('upon request: ' + roster.logins[1].printNames[2]);
+  			var p = [];
+  			var temp = [];
+  			for (var i=0; i<roster.logins.length; i++){
+  				p = roster.logins[i].printNames;
+  				temp[i]={ type: roster.logins[i].type, owner: roster.logins[i].owner, printNames: p };
+  			}
+    		return temp;
+  		});
+  		nutella.net.subscribe('set_roster', function (message, from){
+    		roster.logins = [];
+			for(var i=0; i < message.length; i++){
+    			roster.logins.push({type:message[i].type, owner: message[i].owner, printNames: message[i].printNames});
+    		}
+    		console.log('from client: ' + roster.logins[1].printNames[2]);
+    		roster.save();
+    		console.log('after save: ' + roster.logins[1].printNames[2]);
+  		});
+
+  });
+
+function reset_roster () {
+  roster['logins']=[];
+  roster.save();
+};
 
 function reset_channels () {
   channels['channel']=[];
