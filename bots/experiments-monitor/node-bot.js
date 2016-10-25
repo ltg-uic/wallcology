ca6911bf50e5e2e422677b1ea0693edd679bd42f
tmoher <moher@toms-laptop.local>
2016-10-24 20:06:59 -0500

    icons (temps)

--- bots/experiments-monitor/node-bot.js
+++ bots/experiments-monitor/node-bot.js
@@ -15,6 +15,7 @@ experiments.load(function(){
         experiments['data'] = [[],[],[],[],[]]; experiments.save();       
     };
     console.log ( experiments['data'].length);
+    
     nutella.net.handle_requests('get_experiments',function(group, from) {
         var response = []; console.log ( experiments['data'].length);
         for (var q=0; q < experiments.data[group].length; q++){
